d5fb1880a3112c1ffed2f7b5188d96de1aab64af
PerpetualWar <perpetualwarr@gmail.com>
2018-10-14 11:54:26 +0200

    fix

--- database/seeds/initial.js
+++ database/seeds/initial.js
@@ -3,14 +3,14 @@ exports.seed = async function(knex, Promise) {
   return Promise.all([
     await knex('guilds').del(),
     // Inserts seed entries
-    await knex('guilds').insert({
+    knex('guilds').insert({
       id: '150610549829271552',
       name: '#capickup',
     }),
 
     await knex('channels').del(),
     // Inserts seed entries
-    await knex('channels').insert({
+    knex('channels').insert({
       id: '150610797117046784',
       name: '#capickup',
       guild_id: '150610549829271552',
@@ -18,7 +18,7 @@ exports.seed = async function(knex, Promise) {
 
     await knex('players').del(),
     // Inserts seed entries
-    await knex('players').insert({
+    knex('players').insert({
       id: '103562232985571328',
       username: 'PerpetualWar',
     }),
